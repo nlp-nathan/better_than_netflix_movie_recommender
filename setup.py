d4474c418e736e54a9396644be201831a3313426
nathann3 <59952641+nathann3@users.noreply.github.com>
2021-07-22 12:25:59 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=': Use state-of-the-art models to recommend movies.',
+    author='Nathan Nguyen',
+    license='',
+)

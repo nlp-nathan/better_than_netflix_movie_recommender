955c933160d4ebd2d37351a85b431e44b71677c7
nathann3 <59952641+nathann3@users.noreply.github.com>
2021-08-09 17:41:12 -0700

    Update setup files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description=': Use state-of-the-art models to recommend movies.',
+    description=': LightGCN and NGCF: State-of-the-art models to recommend movies',
     author='Nathan Nguyen',
     license='',
 )
